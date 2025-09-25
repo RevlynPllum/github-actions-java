a47eadfd7ef2fd68b65e743a431697e2defb94d1
RevlynPllum <revlynschreiber@outlook.com>
2025-09-25 14:52:52 +0200

    Update DevopsTrainingApplication.java

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -3,7 +3,6 @@ package com.example.devopstraining;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class DevopsTrainingApplication {
 
