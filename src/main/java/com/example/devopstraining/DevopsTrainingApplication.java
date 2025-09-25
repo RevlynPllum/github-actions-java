a8aad64aa28575a8c756a55bb958c43a8289c724
RevlynPllum <revlynschreiber@outlook.com>
2025-09-25 14:48:03 +0200

    Update DevopsTrainingApplication.java

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -1,18 +1,12 @@
+package com.example.devopstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class DevopsTrainingApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DevopsTrainingApplication.class, args);
-    }
-}
 
-public class DevopsTrainingApplication {
-    
-    // Private constructor to prevent instantiation
-    private DevopsTrainingApplication() {
-        throw new UnsupportedOperationException("Utility class");
-    }
-    
     public static void main(String[] args) {
         SpringApplication.run(DevopsTrainingApplication.class, args);
     }
